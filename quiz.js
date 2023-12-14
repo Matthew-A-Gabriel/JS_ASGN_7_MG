9faa85754148db8fe87b9fd7ea178a301e855c4d
StudentAM <studentam@Administrators-MacBook-Pro.local>
2023-12-14 10:26:08 -0800

    more JS and CSS done

---
+++ quiz.js
@@ -0,0 +1,41 @@
+
+let quizAnswersQ1 = {
+   ans1: true,
+   ans2: false,
+   ans3: false,
+   ans4: false,
+
+};
+let quizAnswersQ2 = {
+   ans1: false,
+   ans2: true,
+   ans3: false,
+   ans4: false,
+
+};
+let quizAnswersQ3 = {
+   ans1: false,
+   ans2: false,
+   ans3: true,
+   ans4: false,
+
+};
+let quizAnswersQ4 = {
+   ans1: false,
+   ans2: false,
+   ans3: false,
+   ans4: true,
+
+};
+
+let quizQArray = [
+    quizAnswersQ1,
+    quizAnswersQ2,
+    quizAnswersQ3,
+    quizAnswersQ4,
+];
+
+let ansArr = [
+]
+
+
