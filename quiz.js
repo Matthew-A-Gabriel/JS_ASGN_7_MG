9b27615be34b14aaee7a1384506a5f769badabe8
StudentAM <studentam@Administrators-MacBook-Pro.local>
2023-12-15 10:28:35 -0800

    Some stuff

--- quiz.js
+++ quiz.js
@@ -1,38 +1,41 @@
 
-let quizAnswersQ1 = {
-   ans1: true,
-   ans2: false,
-   ans3: false,
-   ans4: false,
+let quizAnswersQ1 = [
+   "1a",
+   "1b",
+   "1c",
+   "1d",
+];
 
-};
-let quizAnswersQ2 = {
-   ans1: false,
-   ans2: true,
-   ans3: false,
-   ans4: false,
+let quizAnswersQ2 = [   
+   "2a",
+   "2b",
+   "2c",
+   "2d",
+];
 
-};
-let quizAnswersQ3 = {
-   ans1: false,
-   ans2: false,
-   ans3: true,
-   ans4: false,
+let quizAnswersQ3 = [   
+   "3a",
+   "3b",
+   "3c",
+   "3d",
+];
 
-};
-let quizAnswersQ4 = {
-   ans1: false,
-   ans2: false,
-   ans3: false,
-   ans4: true,
-
-};
+let quizAnswersQ4 = [   
+   "4a",
+   "4b",
+   "4c",
+   "4d",
+];
 
 let quizQArray = [
     quizAnswersQ1,
     quizAnswersQ2,
     quizAnswersQ3,
     quizAnswersQ4,
+];
+
+let ansArray = [
+
 ];
 
 let ansArr = [
